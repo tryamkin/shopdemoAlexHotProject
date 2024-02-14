20f9991f70cb793e26deda59579dc3241cc00124
tryam <tryamkin@gmail.com>
2024-02-14 19:22:55 +0200

    verify pom

--- src/main/java/org/itfriendly/common/CommonActions.java
+++ src/main/java/org/itfriendly/common/CommonActions.java
@@ -5,10 +5,10 @@ public class CommonActions {
     public static boolean checkOsForGitHub() {
         // для запуска на GitHub CI
         System.out.println(Config.OS_NAME_FOR_GIT);
-        if (!Config.OS_NAME_FOR_GIT.contains("Linux")) {
-            return false;
-        } else {
+        if (Config.OS_NAME_FOR_GIT.contains("Linux")) {
             return true;
+        } else {
+            return false;
         }
     }
 }
--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -14,6 +14,7 @@ import java.time.Duration;
 import static org.itfriendly.common.CommonActions.checkOsForGitHub;
 
 import static org.itfriendly.common.Config.BROWSER_NAME;
+import static org.itfriendly.common.Config.OS_NAME_FOR_GIT;
 import static org.itfriendly.constants.Constatnt.TimeoutVariables.IMPLISITY_WAIT;
 import static org.itfriendly.constants.Constatnt.TimeoutVariables.PAGELOAD_WAIT;
 
@@ -25,26 +26,28 @@ abstract public class BaseSeleniumTest {
     public void setUp() {
 
     //  createDriver(driver, BROWSER_NAME);
-        switch (BROWSER_NAME) {
-            case "CHROME" -> {
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-            }
-            case "FIREFOX" -> {
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-            }
-            default -> System.out.println("Wrong browser name" + BROWSER_NAME);
+//        switch (BROWSER_NAME) {
+//            case "CHROME" -> {
+//                WebDriverManager.chromedriver().setup();
+//                driver = new ChromeDriver();
+//            }
+//            case "FIREFOX" -> {
+//                WebDriverManager.firefoxdriver().setup();
+//                driver = new FirefoxDriver();
+//            }
+//            default -> System.out.println("Wrong browser name" + BROWSER_NAME);
+//
+//        }
 
-        }
-
-        if (checkOsForGitHub()){
+        if (!OS_NAME_FOR_GIT.equals("Linux")){
+            driver = new ChromeDriver();
+          }else{
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless","--no-sandbox",
                     "--disable-gpu","--disable-dev-shm-usage",
                     "--window-size=1920,1080","--remote-allow-origins=*");
             driver = new ChromeDriver(options);
-      }
+            }
 
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGELOAD_WAIT));
