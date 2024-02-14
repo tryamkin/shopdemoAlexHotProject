be594f77bcef6be01d5d56d5d2aa93ea7eb1cd67
tryam <tryamkin@gmail.com>
2024-02-14 21:14:49 +0200

    test Firefox + screenshot

--- src/main/java/org/itfriendly/common/Config.java
+++ src/main/java/org/itfriendly/common/Config.java
@@ -8,7 +8,7 @@ public class Config {
      * ETC ...
      *
      */
-    public static final String BROWSER_NAME="FIREFOX";
+    public static final String BROWSER_NAME="CHROME";
     /*
      for github action, он стартует на линуксе, и эта строка определяет операционку и далее хром запускается с ключём --headless (с запуском на вируальном мониторе)
      */
