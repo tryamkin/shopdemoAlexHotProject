75f4b0d1a6cf503517f43ef3a10f1641972d8088
tryam <tryamkin@gmail.com>
2024-02-14 21:12:55 +0200

    test Firefox + screenshot

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -12,7 +12,7 @@ public class MainPageTest extends BaseSeleniumTest {
     public void openPage(){
         MainPage mainPage = new MainPage();
         mainPage.openPage();
-        Assert.assertEquals(mainPage.logoName(),"MERN Store");
+        Assert.assertEquals(mainPage.logoName(),"ERN Store");
 
 
     }
