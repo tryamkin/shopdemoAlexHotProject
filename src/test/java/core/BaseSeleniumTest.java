34149de70c13714fa64c9bd3d13216969cdeefec
tryam <tryamkin@gmail.com>
2024-02-14 21:12:02 +0200

    test Firefox + screenshot

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -21,33 +21,28 @@ abstract public class BaseSeleniumTest {
 
     @BeforeClass
     public void setUp() {
-
-
-        if (!OS_NAME_FOR_GIT.equals("Linux")){
-           switch (BROWSER_NAME) {
-                case "CHROME" :
+        if (!OS_NAME_FOR_GIT.equals("Linux")) {
+            switch (BROWSER_NAME) {
+                case "CHROME":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
-                break;
-
-                case "FIREFOX" :
+                    break;
+                case "FIREFOX":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
-                break;
+                    break;
                 default:
                     System.out.println("Wrong browser name" + BROWSER_NAME);
 
             }
-          }
-        else if (BROWSER_NAME.equals("CHROME")){
+        } else if (BROWSER_NAME.equals("CHROME")) {
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless","--no-sandbox",
-                    "--disable-gpu","--disable-dev-shm-usage",
-                    "--window-size=1920,1080","--remote-allow-origins=*");
+            options.addArguments("--headless", "--no-sandbox",
+                    "--disable-gpu", "--disable-dev-shm-usage",
+                    "--window-size=1920,1080", "--remote-allow-origins=*");
             driver = new ChromeDriver(options);
             System.out.println("Chrome");
-            }
-        else {
+        } else {
             FirefoxOptions options = new FirefoxOptions();
             options.addArguments("--headless");
             driver = new FirefoxDriver(options);
