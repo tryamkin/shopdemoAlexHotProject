726995dd973c4683c80acb87e8d332f79abfa249
tryam <tryamkin@gmail.com>
2024-02-21 16:31:33 +0200

    fix yml

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -23,14 +23,14 @@ abstract public class BaseSeleniumTest {
     public void setUp() {
 
         /**
-         * тут мы проверяем - если операционка линукс - то применяем настройки для гита. Основная это ключ (--headless)
+         * Тут мы проверяем - если операционка линукс - то применяем настройки для гита. Основная это ключ (--headless)
          * которая запускает тесты на виртуальном мониторе
          */
-        if (OS_NAME_FOR_GIT.equals("Linux")) {
-            driver = gitRunConfig(driver, BROWSER_NAME);
-        } else {
-            driver = chooseDriver(driver, BROWSER_NAME);
-        }
+//        if (OS_NAME_FOR_GIT.equals("Linux")) {
+//            driver = gitRunConfig(driver, BROWSER_NAME);
+//        } else {
+//            driver = chooseDriver(driver, BROWSER_NAME);
+//        }
 
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGELOAD_WAIT));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLISITY_WAIT));
