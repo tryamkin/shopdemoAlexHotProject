1780cd7346c94dd367343d3b2c312b1e9991f04d
tryam <tryamkin@gmail.com>
2024-02-22 14:53:19 +0200

    use driver version in ci tests

--- src/main/java/org/itfriendly/common/Config.java
+++ src/main/java/org/itfriendly/common/Config.java
@@ -44,7 +44,7 @@ public class Config {
     public static WebDriver gitRunConfig(WebDriver driver, String browser) {
         switch (browser) {
             case "CHROME":
-                WebDriverManager.firefoxdriver().setup();
+                WebDriverManager.chromedriver().driverVersion("121").setup();
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--headless", "--no-sandbox",
                         "--disable-gpu", "--disable-dev-shm-usage",
