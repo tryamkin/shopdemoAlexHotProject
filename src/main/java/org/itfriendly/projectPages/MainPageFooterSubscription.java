98b65851fca6c111363ba4d953a62e1999f43906
Olena Kharkova <110603507+Olena-Kharkova@users.noreply.github.com>
2024-02-24 21:00:58 +0200

    Lena tests (#6)
    * Lena's tests
    
    * Lena's tests
    
    * Lena's tests

---
+++ src/main/java/org/itfriendly/projectPages/MainPageFooterSubscription.java
@@ -0,0 +1,136 @@
+package org.itfriendly.projectPages;
+
+import org.itfriendly.core.BaseSeleniumPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Random;
+
+import static org.itfriendly.constants.Constatnt.Urls.SHOP_DEMO_URL;
+
+
+public class MainPageFooterSubscription extends BaseSeleniumPage {
+    @FindBy(xpath = "//*[@class=\"logo\"]")
+    private WebElement logo;
+    @FindBy(xpath = "//h3[text()='Newsletter']")
+    private WebElement subscriptionSectionName;
+    @FindBy(xpath = "//p[text()='Sign Up for Our Newsletter']")
+    private WebElement subscriptionInputFieldName;
+    @FindBy(xpath = "//input[contains(@placeholder,'Please Enter Your Email')]")
+    private WebElement subscriptionPlaceholderName;
+    @FindBy(xpath = "//span[text()='Subscribe']")
+    private WebElement subscriptionButtonName;
+    @FindBy(xpath = "//input[contains(@placeholder,'Please Enter Your Email')]")
+    private WebElement subscriptionInputField;
+    @FindBy(xpath ="//span[@class='invalid-message' and contains(text(),'Email format is invalid.')]")
+    private WebElement errorMessageForInvalidEmail;
+    @FindBy(xpath ="//span[@class='invalid-message' and contains(text(),'Email is required.')]")
+    private WebElement emptyEmailSubscription;
+
+    private String errorMessageElement = "Email is already subscribed.";
+    //How to catch the pop-up or message window in Chrome:
+    //Open devTools (F12).
+    //Select the "Sources" tab.
+    //While the element you want is displayed, press F8 (or Ctrl+/). This will break script execution and "freeze" the DOM exactly as it is displayed.
+    //From this point, use Ctrl+Shift+C to select the element.
+    @FindBy(xpath = "//h4[text()='You have successfully subscribed to the newsletter']")
+    private WebElement successMessageSubscription;
+    //constructor - done
+    public MainPageFooterSubscription() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void openPage() {
+        driver.get(SHOP_DEMO_URL);
+    }
+
+    public String subscriptionSectionName() {
+        waitForElementVisibility(subscriptionSectionName);
+        return subscriptionSectionName.getText();
+    }
+    public String subscriptionInputFieldName() {
+        waitForElementVisibility(subscriptionInputFieldName);
+        return subscriptionInputFieldName.getText();
+    }
+
+    public String subscriptionPlaceholderName() {
+        waitForElementVisibility(subscriptionPlaceholderName);
+        return subscriptionPlaceholderName.getAttribute("placeholder");
+    }
+
+    public String subscriptionButtonName(){
+        waitForElementVisibility(subscriptionButtonName);
+        return subscriptionButtonName.getText();
+    }
+    public boolean isSubscriptionButtonClickable() {
+        waitForElementVisibility(subscriptionButtonName);
+        return isClickable(subscriptionButtonName);
+    }
+
+    private boolean isClickable(WebElement element) {
+        return element.isEnabled() && element.isDisplayed();
+    }
+
+    public WebElement getSubscriptionInputField() {
+        return subscriptionInputField;
+    }
+    public void clickSubscriptionButton() {
+        waitForElementVisibility(subscriptionButtonName);
+        subscriptionButtonName.click();
+    }
+
+    public String isSubscriptionSuccessful() {
+        waitForElementVisibility(successMessageSubscription);
+        return successMessageSubscription.getText();
+    }
+
+
+    public static String generateRandomEmail() {
+        String[] domains = {"example.com", "test.com", "gmail.com", "randommail.org"};
+        String username = generateRandomString(8); // Change the length as needed
+                String domain = domains[new Random().nextInt(domains.length)];
+        return username + "@" + domain;
+    }
+
+    private static String generateRandomString(int length) {
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder randomString = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            randomString.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return randomString.toString();
+    }
+
+    public static String generateRandomInvalidEmail() {
+        String[] invalidEmails = {
+                "invalidemail@",
+                "missingdomain@.com",
+                "missingusername@domain.com",
+                "toomany@@symbols.com",
+                "spaces in email@example.com"
+        };
+
+        return invalidEmails[new Random().nextInt(invalidEmails.length)];
+    }
+
+    public String invalidEmailSubscription(){
+        waitForElementVisibility(errorMessageForInvalidEmail);
+        return errorMessageForInvalidEmail.getText();
+    }
+
+    public String emptyEmailSubscription() {
+        waitForElementVisibility(emptyEmailSubscription);
+        return emptyEmailSubscription.getText();
+    }
+
+    public String getErrorMessageElement() {
+        return errorMessageElement;
+    }
+
+}
+
+
---
+++ src/test/java/tests/HelenMainPageTestFooter.java
@@ -0,0 +1,92 @@
+package tests;
+
+import core.BaseSeleniumTest;
+import org.itfriendly.projectPages.MainPageFooterSubscription;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class HelenMainPageTestFooter extends BaseSeleniumTest {
+
+
+
+    @Test
+    public void subscriptionSectionNameTest() {
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+        mainPageFooterSubscription.openPage();
+        Assert.assertEquals(mainPageFooterSubscription.subscriptionSectionName(), "NEWSLETTER");
+    }
+
+    @Test
+    public void subscriptionInputFieldNameTest() {
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+        mainPageFooterSubscription.openPage();
+        Assert.assertEquals(mainPageFooterSubscription.subscriptionInputFieldName(), "Sign Up for Our Newsletter");
+    }
+
+    @Test
+    public void subscriptionInputFieldContainsPlaceholderTest() {
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+        mainPageFooterSubscription.openPage();
+        Assert.assertEquals(mainPageFooterSubscription.subscriptionPlaceholderName(), "Please Enter Your Email");
+    }
+
+    @Test
+    public void subscriptionButtonNameAndClickableTest() {
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+        mainPageFooterSubscription.openPage();
+        Assert.assertEquals(mainPageFooterSubscription.subscriptionButtonName(), "Subscribe");
+        Assert.assertTrue(mainPageFooterSubscription.isSubscriptionButtonClickable(), "Button isn't clickable");
+    }
+
+    @Test
+    public void subscribeWithValidEmailTest() {
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+        mainPageFooterSubscription.openPage();
+
+        // Step 1: Navigate to the input field "Sign Up for Our Newsletter"
+        WebElement emailInputField = mainPageFooterSubscription.getSubscriptionInputField();
+        // Step 2: Enter a valid email address in the input field
+        String randomEmail = MainPageFooterSubscription.generateRandomEmail();
+        emailInputField.sendKeys(randomEmail);
+        // Step 3: Click on the "Subscribe" button
+        mainPageFooterSubscription.clickSubscriptionButton();
+        // Verification: Check if subscription is successful
+        String successMessage = "You have successfully subscribed to the newsletter";
+        Assert.assertEquals(mainPageFooterSubscription.isSubscriptionSuccessful(), successMessage);
+
+    }
+
+    @Test
+    public void subscribeWithInvalidEmailTest() {
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+        mainPageFooterSubscription.openPage();
+        WebElement emailInputField = mainPageFooterSubscription.getSubscriptionInputField();
+        String invalidEmail = MainPageFooterSubscription.generateRandomInvalidEmail();
+        emailInputField.sendKeys(invalidEmail);
+        mainPageFooterSubscription.clickSubscriptionButton();
+        Assert.assertEquals(mainPageFooterSubscription.invalidEmailSubscription(), "Email format is invalid.");
+    }
+
+    @Test
+    public void subscribeWithEmptyEmailTest(){
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+        mainPageFooterSubscription.openPage();
+        mainPageFooterSubscription.clickSubscriptionButton();
+        Assert.assertEquals(mainPageFooterSubscription.emptyEmailSubscription(), "Email is required.");
+    }
+
+    @Test
+    public void subscribeMultipleTimesTest(){
+        MainPageFooterSubscription mainPageFooterSubscription = new MainPageFooterSubscription();
+
+        for (int i = 0; i <= 1; i++) {
+            subscribeWithValidEmailTest();
+            }
+
+        String errorMessageElement = mainPageFooterSubscription.getErrorMessageElement();
+        //comment because we don't want to fail our tests :)
+        //Assert.assertEquals(errorMessageElement.contains("Email is already subscribed."), "Email is already subscribed.");
+    }
+}
