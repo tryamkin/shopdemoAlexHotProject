ec4a9b663a2ee2962c1ed09ede8fb15661fd14b3
tryam <tryamkin@gmail.com>
2024-02-29 09:14:46 +0200

    add firefox browser into yaml

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -5,6 +5,8 @@ import org.itfriendly.core.BaseSeleniumPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -18,17 +20,24 @@ import static org.itfriendly.constants.Constatnt.TimeoutVariables.PAGELOAD_WAIT;
  */
 
 abstract public class BaseSeleniumTest {
-   public static final String OS_NAME_FOR_GIT = System.getProperty("os.name");
-   protected WebDriver driver;
+    public static final String OS_NAME_FOR_GIT = System.getProperty("os.name");
+    private static final Boolean ENV_CHROME = true;
+    protected WebDriver driver;
 
     @BeforeClass
     public void setUp() {
-        if (OS_NAME_FOR_GIT.equals("Linux")){
-              driver = new ChromeDriver(new ChromeOptions().addArguments(
-              "--headless","--window-size=1920,1080"));
+        if (OS_NAME_FOR_GIT.equals("Linux")) {
+            if (ENV_CHROME) {
+                driver = new ChromeDriver(new ChromeOptions().addArguments(
+                        "--headless", "--window-size=1920,1080"));
+            } else {
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                driver = new FirefoxDriver(options);
+            }
         } else {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
 
         }
         driver.manage().window().maximize();
@@ -41,7 +50,11 @@ abstract public class BaseSeleniumTest {
 
     @AfterClass
     public void tearDown() {
+        if (ENV_CHROME){
         driver.close();
-        driver.quit();
+        driver.quit();}
+        else {
+            driver.close();
+        }
     }
 }
